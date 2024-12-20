494f6d1918b6bdeba4d618b43078fb44c7210e7b
sarnali15 <191sarnali@gmail.com>
2024-12-20 14:46:31 +0600

    access token added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -18,7 +18,7 @@ const loginUser = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: 200,
     success: true,
-    message: 'User is logged in successfully!',
+    message: 'Login successful',
     data: result,
   });
 });
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,8 @@
+import config from '../../config';
 import AppError from '../../errors/AppError';
 import { TLoginUser, TUser } from './user.interface';
 import { User } from './user.model';
+import jwt from 'jsonwebtoken';
 
 const createUserIntoDB = async (payload: TUser) => {
   const result = await User.create(payload);
@@ -9,14 +11,32 @@ const createUserIntoDB = async (payload: TUser) => {
 
 const loginUser = async (payload: TLoginUser) => {
   const user = await User.findOne({ email: payload.email });
-  if (!user) {
-    throw new AppError(404, 'This user is not found !');
+
+  // // checking if the user is blocked
+
+  const userStatus = user?.isBlocked;
+
+  if (userStatus) {
+    throw new AppError(403, 'This user is blocked !');
   }
+
+  // // checking if the password is correct
 
   if (!(await User.isPasswordMatched(payload.password, user?.password))) {
     throw new AppError(403, 'Password do not match !');
   }
-  return {};
+
+  //create token and sent to the  client
+  const jwtPayload = {
+    email: user?.email,
+    role: user?.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+  return {
+    accessToken,
+  };
 };
 
 const getAllUsersFromDB = async () => {
