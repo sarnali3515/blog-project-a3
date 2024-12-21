09127d246c3d70843b216e8877ba066cdafdf8df
sarnali15 <191sarnali@gmail.com>
2024-12-21 21:23:35 +0600

    fixed result

--- src/app/modules/user/admin.service.ts
+++ src/app/modules/user/admin.service.ts
@@ -2,14 +2,16 @@ import { User } from './user.model';
 import AppError from '../../errors/AppError';
 
 const blockUserInDB = async (userId: string) => {
-  // Find the user by ID
   const user = await User.findById(userId);
+
+  if (user?.isBlocked) {
+    throw new AppError(404, 'This user is already blocked');
+  }
 
   if (!user) {
     throw new AppError(404, 'User not found');
   }
 
-  // Update the `isBlocked` property
   user.isBlocked = true;
   await user.save();
 
